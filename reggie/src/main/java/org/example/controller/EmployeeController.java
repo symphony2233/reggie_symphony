d305f84cda276dece9f0e39f6d886f0c70929a91
symphony <2362534639@qq.com>
2024-05-08 21:42:56 +0800

    feat: 员工的登出、crud

--- reggie/src/main/java/org/example/controller/EmployeeController.java
+++ reggie/src/main/java/org/example/controller/EmployeeController.java
@@ -1,17 +1,16 @@
 package org.example.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.example.common.R;
 import org.example.entity.Employee;
 import org.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -72,7 +71,14 @@ public class EmployeeController {
         return R.success(emp);
     }
 
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清理Session中保存的当前登录员工的id
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
 
+    // 保存用户信息
     @PostMapping
     public R<String> save(@RequestBody Employee employee){
         log.info("新增员工，员工信息：{}",employee.toString());
@@ -83,4 +89,44 @@ public class EmployeeController {
         employeeService.save(employee);
         return R.success("新增员工成功");
     }
+
+    // 分页查询
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name){
+        log.info("page = {},pageSize = {},name = {}" ,page,pageSize,name);
+        //构造分页构造器
+        Page pageInfo = new Page(page,pageSize);
+
+        //构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        //添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        //执行查询
+        employeeService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
+
+
+    // 根据id修改员工信息
+    @PutMapping
+    public R<String> update(@RequestBody Employee employee){
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+    }
+
+    // 查询员工
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息...");
+        Employee employee = employeeService.getById(id);
+        if(employee != null){
+            return R.success(employee);
+        }
+        return R.error("没有查询到对应员工信息");
+    }
+
+
 }
